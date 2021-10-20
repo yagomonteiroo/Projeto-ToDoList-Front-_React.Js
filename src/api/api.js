7786cff133331f42d6f98463f9801e7909bb8105
yago.monteiroo@gmail.com <yago.monteiroo@gmail.com>
2021-10-19 23:29:27 -0300

    trocando o bano local pelo de produção

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 const Api = {
-   apiUrl: 'http://localhost:3001/tarefas',
+   apiUrl: 'https://backend-lista-de-tarefas.herokuapp.com/tarefas',
    fetchGetAll: () => fetch(Api.apiUrl),
    fetchGetById: id => fetch(`${Api.apiUrl}/${id}`),
    fetchPost: (tarefa) => {
